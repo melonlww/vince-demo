e6beff5f490de6852832c2976c693388992d9241
weiwei.li <weiwei.li@ximalaya.com>
2020-12-02 18:30:18 +0800

    thread future

---
+++ thread-practice/src/main/java/com/xima/thread/practice/thread_demo/jmm/VisibilityProblem.java
@@ -0,0 +1,46 @@
+package com.xima.thread.practice.thread_demo.jmm;
+
+/**
+ * 描述：     演示可见性带来的问题
+ */
+public class VisibilityProblem {
+    int a = 10;
+    int b = 20;
+
+    private void change() {
+        a = 30;
+        b = a;
+    }
+
+    private void print() {
+        System.out.println("b=" + b + ";a=" + a);
+    }
+
+    public static void main(String[] args) {
+        while (true) {
+            VisibilityProblem problem = new VisibilityProblem();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    problem.change();
+                }
+            }).start();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    problem.print();
+                }
+            }).start();
+        }
+    }
+}
