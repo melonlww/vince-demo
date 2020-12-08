c5bd3cf24f78ae22198408fff4aec2d611b1a0b2
weiwei.li <weiwei.li@ximalaya.com>
2020-12-08 15:43:18 +0800

    spring framework

---
+++ spring-framework-practice/src/main/java/com/xima/App.java
@@ -0,0 +1,13 @@
+package com.xima;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
