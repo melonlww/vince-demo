2a73cee84d8384f47315f956884853d70361e787
weiwei.li <weiwei.li@ximalaya.com>
2020-12-29 18:44:30 +0800

    spring framework & data structure

---
+++ spring-framework-practice/spring-shengsiyuan/src/main/java/com/xima/springframework/shengsiyuan/practice/stage1/bean/Student4.java
@@ -0,0 +1,43 @@
+package com.xima.springframework.shengsiyuan.practice.stage1.bean;
+
+public class Student4 {
+
+    private String name;
+
+    private int age;
+
+    private Teacher teacher;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    @Override
+    public String toString() {
+        return "Student4{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", teacher=" + teacher +
+                '}';
+    }
+}
