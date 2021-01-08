a6f5b51cfa7c0bc1e15a1421683c4326a371685f
weiwei.li <weiwei.li@ximalaya.com>
2021-01-08 19:57:07 +0800

    spring framework

--- spring-framework-practice/spring-ioc/src/main/java/com/xima/springframework/ioc/practice/mashibin/bean/PersonA.java
+++ spring-framework-practice/spring-ioc/src/main/java/com/xima/springframework/ioc/practice/mashibin/bean/PersonA.java
@@ -2,27 +2,7 @@ package com.xima.springframework.ioc.practice.mashibin.bean;
 
 public class PersonA{
 
-    private String name;
-
-    private int age;
-
     private PersonB personB;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 
     public PersonB getPersonB() {
         return personB;
@@ -35,9 +15,7 @@ public class PersonA{
     @Override
     public String toString() {
         return "PersonA{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", personB=" + personB +
+                "personB=" + personB +
                 '}';
     }
 }
--- spring-framework-practice/spring-ioc/src/main/java/com/xima/springframework/ioc/practice/mashibin/bean/PersonB.java
+++ spring-framework-practice/spring-ioc/src/main/java/com/xima/springframework/ioc/practice/mashibin/bean/PersonB.java
@@ -2,27 +2,7 @@ package com.xima.springframework.ioc.practice.mashibin.bean;
 
 public class PersonB {
 
-    private String name;
-
-    private int age;
-
     private PersonA personA;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 
     public PersonA getPersonA() {
         return personA;
@@ -35,9 +15,7 @@ public class PersonB {
     @Override
     public String toString() {
         return "PersonB{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", personA=" + personA +
+                "personA=" + personA +
                 '}';
     }
 }
