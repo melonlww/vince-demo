0886b46dd84b85bbad6d080a47d7d04484494ac5
weiwei.li <weiwei.li@ximalaya.com>
2021-01-11 16:21:58 +0800

    spring framework

--- spring-framework-practice/spring-ioc/src/main/java/com/xima/springframework/ioc/practice/mashibin/SpringClient3_循环依赖以及三级代理.java
+++ spring-framework-practice/spring-ioc/src/main/java/com/xima/springframework/ioc/practice/mashibin/SpringClient3_循环依赖以及三级代理.java
@@ -6,9 +6,13 @@ import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
+ * Bean三级缓存.jpg
  *
+ * 1.一级缓存放成品对象
+ * 2.二级缓存放半成品对象
+ * 3.三级缓存放lambda表达式，来完成代理对象的覆盖过程
  */
-public class SpringClient3_循环依赖 {
+public class SpringClient3_循环依赖以及三级代理 {
 
     public static void main(String[] args) {
         AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext8.xml");
