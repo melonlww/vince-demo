2c2168d13cd6b28ce8ba5983c939f2433f4beead
weiwei.li <weiwei.li@ximalaya.com>
2021-01-13 15:00:36 +0800

    设计模式-单例

--- design-practice/src/main/java/com/xima/design/pattern/单例模式/破坏单例模式/序列化/TestSerializableSingletonFactory.java
+++ design-practice/src/main/java/com/xima/design/pattern/单例模式/破坏单例模式/序列化/TestSerializableSingletonFactory.java
@@ -2,6 +2,16 @@ package com.xima.design.pattern.单例模式.破坏单例模式.序列化;
 
 import java.io.*;
 
+/**
+ * 序列化：
+ * 把内存中对象的状态转换为字节码的形式
+ * 把字节码通过IO输出流，写到磁盘上
+ * 永久保存下来，持久化
+ *
+ * 反序列化：
+ * 将持久化的字节码内容，通过IO输入流读到内存中来
+ * 转化成一个Java对象
+ */
 public class TestSerializableSingletonFactory {
     public static void main(String[] args) {
         SerializableSingleton s1 = SerializableSingleton.getInstance();
