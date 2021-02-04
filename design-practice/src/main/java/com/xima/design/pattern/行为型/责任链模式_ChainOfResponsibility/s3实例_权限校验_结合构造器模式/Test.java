e5ece4ca32fce40a496fed0c864cc16da217620e
weiwei.li <weiwei.li@ximalaya.com>
2021-02-04 11:32:40 +0800

    设计模式

---
+++ design-practice/src/main/java/com/xima/design/pattern/行为型/责任链模式_ChainOfResponsibility/s3实例_权限校验_结合构造器模式/Test.java
@@ -0,0 +1,9 @@
+package com.xima.design.pattern.行为型.责任链模式_ChainOfResponsibility.s3实例_权限校验_结合构造器模式;
+
+public class Test {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberService();
+        memberService.login("liww","123456");
+    }
+}
