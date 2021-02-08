d5f0f79a02ee35a1e199fb4539221133b2134f19
weiwei.li <weiwei.li@ximalaya.com>
2021-02-08 16:28:31 +0800

    设计模式 引入springboot

---
+++ design-practice/src/main/java/com/xima/design/pattern/行为型/状态模式_State/s4实例_订单_Spring状态机/Test.java
@@ -0,0 +1,15 @@
+package com.xima.design.pattern.行为型.状态模式_State.s4实例_订单_Spring状态机;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Test {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Test.class, args);
+
+
+
+    }
+}
