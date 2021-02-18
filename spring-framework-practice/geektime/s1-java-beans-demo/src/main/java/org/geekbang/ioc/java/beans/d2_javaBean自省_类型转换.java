5208a176e8378cd983da582e7af4ff4273aa987b
weiwei.li <weiwei.li@ximalaya.com>
2021-02-19 00:22:12 +0800

    spring

--- spring-framework-practice/geektime/s1-java-beans-demo/src/main/java/org/geekbang/ioc/java/beans/d2_javaBean自省_类型转换.java
+++ spring-framework-practice/geektime/s1-java-beans-demo/src/main/java/org/geekbang/ioc/java/beans/d2_javaBean自省_类型转换.java
@@ -2,10 +2,9 @@ package org.geekbang.ioc.java.beans;
 
 import org.geekbang.ioc.java.beans.pojo.Person;
 
-import java.beans.BeanInfo;
-import java.beans.IntrospectionException;
-import java.beans.Introspector;
-import java.beans.PropertyEditorSupport;
+import java.beans.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.stream.Stream;
 
 /**
@@ -15,18 +14,25 @@ public class d2_javaBean自省_类型转换 {
 
     public static void main(String[] args) throws IntrospectionException {
         BeanInfo beanInfo = Introspector.getBeanInfo(Person.class, Object.class);
+
+        Person person = new Person();
+
         Stream.of(beanInfo.getPropertyDescriptors())
                 .forEach(propertyDescriptor -> {
-                    Class<?> propertyType = propertyDescriptor.getPropertyType();
-                    String propertyName = propertyDescriptor.getName();
+                    Class<?> propertyType = propertyDescriptor.getPropertyType();String propertyName = propertyDescriptor.getName();
                     //为age字段/属性增加 PropertyEditor
                     if ("age".equals(propertyName)) {
                         //String->Integer
                         propertyDescriptor.setPropertyEditorClass(StringToIntegerPropertyEditor.class);
+                        PropertyEditor propertyEditor = propertyDescriptor.createPropertyEditor(person);
 
+                        Method setAgeMethod = propertyDescriptor.getWriteMethod();
+                        propertyEditor.addPropertyChangeListener(new SetPropertyChangeListener(person, setAgeMethod));
+                        propertyEditor.setAsText("99");
                     }
                 });
 
+        System.out.println(person);
     }
 
     static class StringToIntegerPropertyEditor extends PropertyEditorSupport {
@@ -34,6 +40,29 @@ public class d2_javaBean自省_类型转换 {
         public void setAsText(String text) throws java.lang.IllegalArgumentException {
             Integer value = Integer.valueOf(text);
             setValue(value);
+        }
+    }
+
+    static class SetPropertyChangeListener implements PropertyChangeListener {
+
+        private Object bean;
+        private Method setWriteMethod;
+
+        public SetPropertyChangeListener(Object bean, Method setWriteMethod) {
+            this.bean = bean;
+            this.setWriteMethod = setWriteMethod;
+        }
+
+        @Override
+        public void propertyChange(PropertyChangeEvent event) {
+            PropertyEditor source = (PropertyEditor) event.getSource();
+            try {
+                setWriteMethod.invoke(bean, source.getValue());
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
