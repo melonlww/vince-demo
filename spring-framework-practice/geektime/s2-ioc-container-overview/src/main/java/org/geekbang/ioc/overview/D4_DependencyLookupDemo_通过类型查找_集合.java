9c6be15ae78515f7cd7324e37bf31d8d714c6b44
weiwei.li <weiwei.li@ximalaya.com>
2021-02-22 19:20:56 +0800

    spring

---
+++ spring-framework-practice/geektime/s2-ioc-container-overview/src/main/java/org/geekbang/ioc/overview/D4_DependencyLookupDemo_通过类型查找_集合.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.geekbang.ioc.overview;
+
+import org.geekbang.ioc.overview.domain.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Map;
+
+/**
+ * 依赖查找示例
+ * 1. 通过名称的方式来查找
+ */
+public class D4_DependencyLookupDemo_通过类型查找_集合 {
+
+    public static void main(String[] args) {
+        // 配置 XML 配置文件
+        // 启动 Spring 应用上下文
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-lookup-context2.xml");
+        lookupCollectionByType(beanFactory);
+    }
+
+    private static void lookupCollectionByType(BeanFactory beanFactory) {
+        if (beanFactory instanceof ListableBeanFactory) {
+            ListableBeanFactory listableBeanFactory = (ListableBeanFactory) beanFactory;
+            Map<String, User> users = listableBeanFactory.getBeansOfType(User.class);
+            System.out.println("查找到的所有的 User 集合对象：" + users);
+        }
+    }
+}
