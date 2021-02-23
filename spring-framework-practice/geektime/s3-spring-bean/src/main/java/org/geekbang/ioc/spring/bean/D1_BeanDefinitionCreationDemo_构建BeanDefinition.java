ba341f64efe4590aa338f7d9f73381d1a350484b
weiwei.li <weiwei.li@ximalaya.com>
2021-02-23 16:34:19 +0800

    spring

--- spring-framework-practice/geektime/s3-spring-bean/src/main/java/org/geekbang/ioc/spring/bean/D1_BeanDefinitionCreationDemo_构建BeanDefinition.java
+++ spring-framework-practice/geektime/s3-spring-bean/src/main/java/org/geekbang/ioc/spring/bean/D1_BeanDefinitionCreationDemo_构建BeanDefinition.java
@@ -26,32 +26,37 @@ import org.springframework.beans.factory.support.GenericBeanDefinition;
  * {@link org.springframework.beans.factory.config.BeanDefinition} 构建示例
  *
  */
-public class D1_BeanDefinitionCreationDemo {
+public class D1_BeanDefinitionCreationDemo_构建BeanDefinition {
 
     public static void main(String[] args) {
-        User user = null;
-
-        // 1.通过 BeanDefinitionBuilder 构建
+        /**
+         * 构建方式1
+         * 通过 BeanDefinitionBuilder 构建
+         */
         BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(User.class);
         // 通过属性设置
         beanDefinitionBuilder
                 .addPropertyValue("id", 1)
-                .addPropertyValue("name", "小马哥");
+                .addPropertyValue("name", "小啊宝");
         // 获取 BeanDefinition 实例
         BeanDefinition beanDefinition = beanDefinitionBuilder.getBeanDefinition();
         // BeanDefinition 并非 Bean 终态，可以自定义修改
 
-        // 2. 通过 AbstractBeanDefinition 以及派生类
+
+        /**
+         * 构建方式2
+         * 通过 AbstractBeanDefinition 以及派生类
+         */
         GenericBeanDefinition genericBeanDefinition = new GenericBeanDefinition();
         // 设置 Bean 类型
         genericBeanDefinition.setBeanClass(User.class);
         // 通过 MutablePropertyValues 批量操作属性
         MutablePropertyValues propertyValues = new MutablePropertyValues();
 //        propertyValues.addPropertyValue("id", 1);
-//        propertyValues.addPropertyValue("name", "小马哥");
+//        propertyValues.addPropertyValue("name", "小啊宝");
         propertyValues
-                .add("id", 1)
-                .add("name", "小马哥");
+                .add("id", 2)
+                .add("name", "小啊宝");
         // 通过 set MutablePropertyValues 批量操作属性
         genericBeanDefinition.setPropertyValues(propertyValues);
     }
