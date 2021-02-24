c853633ea234f6481dc0ddbd52074c7d4b5c4f18
weiwei.li <weiwei.li@ximalaya.com>
2021-02-24 11:57:46 +0800

    spring

--- spring-framework-practice/geektime/s3-spring-bean/src/main/java/org/geekbang/ioc/spring/bean/D10_SpecialBeanInstantiationDemo_Bean实例化_特殊方式_演示ServiceLoader使用.java
+++ spring-framework-practice/geektime/s3-spring-bean/src/main/java/org/geekbang/ioc/spring/bean/D10_SpecialBeanInstantiationDemo_Bean实例化_特殊方式_演示ServiceLoader使用.java
@@ -18,6 +18,7 @@ package org.geekbang.ioc.spring.bean;
 
 import org.geekbang.ioc.spring.bean.factory.DefaultUserFactory;
 import org.geekbang.ioc.spring.bean.factory.UserFactory;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -27,27 +28,11 @@ import java.util.ServiceLoader;
 
 /**
  * 特殊的 Bean 实例化示例
- *
  */
-public class D10_SpecialBeanInstantiationDemo_Bean实例化_特殊方式_通过ServiceLoader {
+public class D10_SpecialBeanInstantiationDemo_Bean实例化_特殊方式_演示ServiceLoader使用 {
 
     public static void main(String[] args) {
-        // 配置 XML 配置文件
-        // 启动 Spring 应用上下文
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:/META-INF/special-bean-instantiation-context.xml");
-        // 通过 ApplicationContext 获取 AutowireCapableBeanFactory
-        AutowireCapableBeanFactory beanFactory = applicationContext.getAutowireCapableBeanFactory();
-
-        ServiceLoader<UserFactory> serviceLoader = beanFactory.getBean("userFactoryServiceLoader", ServiceLoader.class);
-
-        displayServiceLoader(serviceLoader);
-
-//        demoServiceLoader();
-
-        // 创建 UserFactory 对象，通过 AutowireCapableBeanFactory
-        UserFactory userFactory = beanFactory.createBean(DefaultUserFactory.class);
-        System.out.println(userFactory.createUser());
-
+        demoServiceLoader();
     }
 
     public static void demoServiceLoader() {
