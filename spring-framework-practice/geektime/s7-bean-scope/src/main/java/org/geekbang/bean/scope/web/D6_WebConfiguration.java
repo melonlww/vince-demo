f9f46365a3059da9bc54ef29c4883ac974fad88e
weiwei.li <weiwei.li@ximalaya.com>
2021-03-18 19:04:41 +0800

    spring

---
+++ spring-framework-practice/geektime/s7-bean-scope/src/main/java/org/geekbang/bean/scope/web/D6_WebConfiguration.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.geekbang.bean.scope.web;
+
+import org.geekbang.ioc.overview.domain.User6;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * 增加Web MVC 配置类
+ *
+ * maven打war包:
+ *      cd ~/practice/code/vince-demo/spring-framework-practice/geektime
+ *      mvn clean package
+ *
+ *      cd ~/practice/code/vince-demo/spring-framework-practice/geektime/s7-bean-scope/target
+ *      java -jar s7-bean-scope-1.0-SNAPSHOT-war-exec.jar
+ *
+ */
+@Configuration
+@EnableWebMvc
+public class D6_WebConfiguration {
+
+    @Bean
+//    @RequestScope
+//    @SessionScope
+    @ApplicationScope
+    public User6 user() {
+        User6 user = new User6();
+        user.setId(1L);
+        user.setName("小ww");
+        return user;
+    }
+
+}
