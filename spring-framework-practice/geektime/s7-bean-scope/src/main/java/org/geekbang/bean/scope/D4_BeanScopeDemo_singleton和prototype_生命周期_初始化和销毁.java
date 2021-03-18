bcc8b1ec2ca1293cb9e6933d38ab96ff877d2394
weiwei.li <weiwei.li@ximalaya.com>
2021-03-18 17:03:13 +0800

    spring

--- spring-framework-practice/geektime/s7-bean-scope/src/main/java/org/geekbang/bean/scope/D4_BeanScopeDemo_singleton和prototype_生命周期_初始化和销毁.java
+++ spring-framework-practice/geektime/s7-bean-scope/src/main/java/org/geekbang/bean/scope/D4_BeanScopeDemo_singleton和prototype_生命周期_初始化和销毁.java
@@ -16,7 +16,7 @@
  */
 package org.geekbang.bean.scope;
 
-import org.geekbang.ioc.overview.domain.User5;
+import org.geekbang.ioc.overview.domain.User6;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -39,64 +39,91 @@ import java.util.Map;
  *  (看打印的beanScopeDemo.users)
  *
  *
+ * 结论三：
+ *  无论是 Singleton 还是 Prototype Bean 均会执行初始化方法回调
+ *  不过仅 Singleton Bean 会执行销毁方法回调
+ *
  *
  *
  * 输出：
  * =======================================
- *
+ * User Bean [singletonUser] 初始化...
+ * User Bean [prototypeUser] 初始化...
+ * User Bean [prototypeUser] 初始化...
+ * User Bean [prototypeUser] 初始化...
+ * User Bean [prototypeUser] 初始化...
+ * 依赖查找：
+ * singletonUser = User5{id=76399353465365, name='null', city=null, workCities=null, lifeCities=null, configFileLocation=null}
+ * User Bean [prototypeUser] 初始化...
+ * prototypeUser = User5{id=76399414918917, name='null', city=null, workCities=null, lifeCities=null, configFileLocation=null}
+ * singletonUser = User5{id=76399353465365, name='null', city=null, workCities=null, lifeCities=null, configFileLocation=null}
+ * User Bean [prototypeUser] 初始化...
+ * prototypeUser = User5{id=76399417014053, name='null', city=null, workCities=null, lifeCities=null, configFileLocation=null}
+ * singletonUser = User5{id=76399353465365, name='null', city=null, workCities=null, lifeCities=null, configFileLocation=null}
+ * User Bean [prototypeUser] 初始化...
+ * prototypeUser = User5{id=76399417467678, name='null', city=null, workCities=null, lifeCities=null, configFileLocation=null}
+ * =======================
+ * 依赖注入：
+ * beanScopeDemo.singletonUser = User5{id=76399353465365, name='null', city=null, workCities=null, lifeCities=null, configFileLocation=null}
+ * beanScopeDemo.singletonUser1 = User5{id=76399353465365, name='null', city=null, workCities=null, lifeCities=null, configFileLocation=null}
+ * beanScopeDemo.prototypeUser = User5{id=76399362011043, name='null', city=null, workCities=null, lifeCities=null, configFileLocation=null}
+ * beanScopeDemo.prototypeUser1 = User5{id=76399363300340, name='null', city=null, workCities=null, lifeCities=null, configFileLocation=null}
+ * beanScopeDemo.prototypeUser2 = User5{id=76399364215530, name='null', city=null, workCities=null, lifeCities=null, configFileLocation=null}
+ * beanScopeDemo.users = {singletonUser=User5{id=76399353465365, name='null', city=null, workCities=null, lifeCities=null, configFileLocation=null}, prototypeUser=User5{id=76399370132993, name='null', city=null, workCities=null, lifeCities=null, configFileLocation=null}}
+ * User Bean [singletonUser] 销毁中...
  *
  */
-public class D4_BeanScopeDemo_singleton和prototype_通过依赖注入_Map集合 {
+public class D4_BeanScopeDemo_singleton和prototype_生命周期_初始化和销毁 {
 
     /**
      * 默认 scope 就是 "singleton"
      */
     @Bean
-    public static User5 singletonUser() {
+    public static User6 singletonUser() {
         return createUser();
     }
 
     @Bean
     @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-    public static User5 prototypeUser() {
+    public static User6 prototypeUser() {
         return createUser();
     }
 
-    private static User5 createUser() {
-        User5 user = new User5();
+    private static User6 createUser() {
+        User6 user = new User6();
         user.setId(System.nanoTime());
         return user;
     }
 
     @Autowired
     @Qualifier("singletonUser")
-    private User5 singletonUser;
+    private User6 singletonUser;
 
     @Autowired
     @Qualifier("singletonUser")
-    private User5 singletonUser1;
+    private User6 singletonUser1;
 
     @Autowired
     @Qualifier("prototypeUser")
-    private User5 prototypeUser;
+    private User6 prototypeUser;
 
     @Autowired
     @Qualifier("prototypeUser")
-    private User5 prototypeUser1;
+    private User6 prototypeUser1;
 
     @Autowired
     @Qualifier("prototypeUser")
-    private User5 prototypeUser2;
+    private User6 prototypeUser2;
 
     @Autowired
-    private Map<String, User5> users;
+    private Map<String, User6> users;
 
     public static void main(String[] args) {
 
         // 创建 BeanFactory 容器
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
         // 注册 Configuration Class（配置类） -> Spring Bean
-        applicationContext.register(D4_BeanScopeDemo_singleton和prototype_通过依赖注入_Map集合.class);
+        applicationContext.register(D4_BeanScopeDemo_singleton和prototype_生命周期_初始化和销毁.class);
         // 启动 Spring 应用上下文
         applicationContext.refresh();
         System.out.println("依赖查找：");
@@ -115,18 +142,18 @@ public class D4_BeanScopeDemo_singleton和prototype_通过依赖注入_Map集合
             /**
              * singletonUser 是共享 Bean 对象
              */
-            User5 singletonUser = applicationContext.getBean("singletonUser", User5.class);
+            User6 singletonUser = applicationContext.getBean("singletonUser", User6.class);
             System.out.println("singletonUser = " + singletonUser);
             /**
              * prototypeUser 是每次依赖查找均生成了新的 Bean 对象
              */
-            User5 prototypeUser = applicationContext.getBean("prototypeUser", User5.class);
+            User6 prototypeUser = applicationContext.getBean("prototypeUser", User6.class);
             System.out.println("prototypeUser = " + prototypeUser);
         }
     }
 
     private static void scopedBeansByInjection(AnnotationConfigApplicationContext applicationContext) {
-        D4_BeanScopeDemo_singleton和prototype_通过依赖注入_Map集合 beanScopeDemo = applicationContext.getBean(D4_BeanScopeDemo_singleton和prototype_通过依赖注入_Map集合.class);
+        D4_BeanScopeDemo_singleton和prototype_生命周期_初始化和销毁 beanScopeDemo = applicationContext.getBean(D4_BeanScopeDemo_singleton和prototype_生命周期_初始化和销毁.class);
 
         System.out.println("beanScopeDemo.singletonUser = " + beanScopeDemo.singletonUser);
         System.out.println("beanScopeDemo.singletonUser1 = " + beanScopeDemo.singletonUser1);
